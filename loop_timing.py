6390e04dfcf034feec9c108badb4262bbedfc1b7
dudi-w <dudi.w9453@gmail.com>
2023-10-21 20:30:34 +0300

    fix bug

--- loop_timing.py
+++ loop_timing.py
@@ -1,16 +1,15 @@
 import time
 
+
 class loopTiming:
     def __init__(self, updates_per_second: float) -> None:
         self._required_loop_duration = updates_per_second
         self._loop_start_time = time.time()
 
     def end_loop(self):
-        time.sleep(0.1)
-        return
-        # current_loop_duration = time.time() - self._loop_start_time
+        current_loop_duration = time.time() - self._loop_start_time
 
-        # if current_loop_duration < self._required_loop_duration:
-        #     time.sleep(self._required_loop_duration - current_loop_duration)
+        if current_loop_duration < self._required_loop_duration:
+            time.sleep(self._required_loop_duration - current_loop_duration)
 
-        # self._loop_start_time = time.time()
+        self._loop_start_time = time.time()
